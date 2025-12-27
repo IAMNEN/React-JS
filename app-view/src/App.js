b20f538a271a0d964d762335019971a6354060f5
Nen Pachani <nenpanchani@gmail.com>
2025-12-27 22:13:59 +0530

    feat: initialize React app with Tailwind CSS and essential files
    - Added logo512.png and logo.svg for application branding.
    - Created manifest.json for PWA support.
    - Included robots.txt for search engine directives.
    - Set up App component with basic styling in App.css.
    - Implemented index.js to render the App component.
    - Added reportWebVitals.js for performance tracking.
    - Configured Tailwind CSS in tailwind.config.js.
    - Established testing setup with setupTests.js and App.test.js.
    - Included index.css for Tailwind directives.

---
+++ app-view/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Top from './components/top'
+
+const App = () => {
+  return (
+    <Top />
+  )
+}
+
+export default App
---
+++ app-view/src/components/top.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const top = () => {
+  return (
+    <div className='top'>
+        <div className='container'>
+            <h2 className='main1'>Let's Become</h2>
+            <h2>More Productive</h2>
+        </div>
+    </div>
+  )
+}
+
+export default top
